37e2231ceef033b03185279a06a976333a0d9bdd
Igor Gabriel <souzaamorimdasilvai@gmail.com>
2024-07-28 20:09:22 -0300

    End

---
+++ Bank/Entities/SavingsAccount.cs
@@ -0,0 +1,27 @@
+﻿namespace Bank.Entities
+{
+    class SavingsAccount : Account
+    {
+        public double InterestRate { get; private set; }
+
+        public SavingsAccount() { }
+
+        public SavingsAccount(int number, string holder, double balance, double interestRate) : base(number, holder, balance)
+        {
+            InterestRate = interestRate;
+        }
+
+        public void UpdateBalance()
+        {
+            Balance += Balance * InterestRate;
+        }
+
+        public override void WithDraw(double amount)
+        {
+            if (Balance <= amount) {
+                base.WithDraw(amount);
+                Balance -= 2.0;
+            }
+        }
+    }
+}
---
+++ Bank/Program.cs
@@ -0,0 +1,13 @@
+﻿using Bank.Entities;
+
+namespace Bank
+{
+    class Program
+    {
+        public static void Main(string[] args) { 
+        
+            BusinessAccount businessAccount = new BusinessAccount();
+
+        }
+    }
+}
